acba7d7448f6779f61773c2527920f63736b673d
Melon <84119783+AffableMelon@users.noreply.github.com>
2024-01-06 09:58:20 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(mylist, idx):
-    if idx < 0 and idx >= len(mylist):
+    if idx < 0 or idx >= len(mylist):
         return None
     elif idx >= 0:
         return (mylist[idx])
