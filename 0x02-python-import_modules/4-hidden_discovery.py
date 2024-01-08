19fb11ee3e5ac1cb24fcfc11e0697b1dbd22523a
melon <affabmelon@gmail.com>
2024-01-08 09:22:49 -0800

    m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    import dis
+    import types
     for name in sorted(hidden_4.__dict__):
         if not name.startswith('__'):
             print(name)
