0fe05f41d0e2dd02ea97d52bbb474dac2a67077e
melon <affabmelon@gmail.com>
2024-01-08 09:15:24 -0800

    m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-
+    import hidden_4
     name = dir(hidden_4)
     for names in name:
         if name[:2] != "__":
-            print(name)
+            rint(name)
