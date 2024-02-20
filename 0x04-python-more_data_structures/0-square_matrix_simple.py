9199bec8fd2e6020b79a497cb2f5ecdcfef31ba8
melon <affabmelon@gmail.com>
2024-02-19 22:52:58 -0800

    m

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newm = []
+    for count in range(len(matrix)):
+        k = matrix[count].copy()
+        for i in range(len(k)):
+            j = (k[i] ** 2)
+            k[i] = j
+        newm.append(k)
+    return(newm)
