71fdaa598e7820fc5dcaa70056c7e9e0d98ca2f6
melon <affabmelon@gmail.com>
2024-02-21 01:48:40 -0800

    m

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    compute = []
+    final = 0
+    if type(roman_string) != str or roman_string is None:
+        return (0)
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in roman_string:
+        compute.append(rom_n[i])
+    check = compute.copy()
+    check.sort(reverse=True)
+    bool = compute == check
+    if bool is True:
+        for i in compute:
+            final += i
+        return(final)
+
+    for i in range(1, len(compute)):
+        if compute[i-1] < compute[i]:
+            sub = compute[i] - compute[i-1]
+            final += sub
+        else:
+            final += compute[i-1]
+    return (final)
