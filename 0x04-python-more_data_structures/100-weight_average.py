8f783fb00e22414ab016dd4b405f08270019cf0e
melon <affabmelon@gmail.com>
2024-02-23 07:40:39 -0800

    m

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum = 0
-    tot = 0
-    for (x, y) in my_list:
-        sum += (x*y)
-        tot += y
-    return (float(sum/tot))
+    try:
+        sum = 0
+        tot = 0
+        for (x, y) in my_list:
+            sum += (x*y)
+            tot += y
+        return (float(sum/tot))
+    except ZeroDivisionError:
+        return (0.00)
