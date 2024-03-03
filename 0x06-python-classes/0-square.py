cb3c78ca2af11fec2be5473afdda979dc650054c
melon <affabmelon@gmail.com>
2024-03-03 02:49:55 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+This file creates a Square class and defines a method square within it
+'''
+class Square:
+    '''empty class square defning a square'''
+    def square (self):
+        pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This file creates a Square class and defines a method square based on
+1. private instance size
+2. init size (none)
+'''
+class Square:
+    """ represent a square"""
+    def __init__(self, size = None):
+        self.__size = size
+    def square (self):
+        pass
